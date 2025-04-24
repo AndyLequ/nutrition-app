b26fe176e3175076aea4ee41275a38a384bbf9e9
Andy Le <andylequ@gmail.com>
2025-04-24 15:47:26 -0500

    fixed issue where the meals added weren't able to be added after the first attempt succeeded. can now add meals more than once

--- app/components/MealPlanList.tsx
+++ app/components/MealPlanList.tsx
@@ -156,7 +156,6 @@ const MealPlanList = () => {
       })
     );
     setSelectedRecipe(null);
-    setSelectedDay("");
     setSearchResults([]);
   };
 
@@ -293,64 +292,5 @@ const MealPlanList = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#f5f5f5",
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-    color: "#333",
-    textAlign: "center",
-  },
-  dayContainer: {
-    backgroundColor: "white",
-    borderRadius: 8,
-    marginBottom: 10,
-    padding: 15,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 2,
-  },
-  dayHeader: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  dayTitle: {
-    fontSize: 18,
-    fontWeight: "600",
-    color: "#2c3e50",
-  },
-  toggleIcon: {
-    fontSize: 16,
-    color: "#7f8c8d",
-  },
-  mealsContainer: {
-    marginTop: 10,
-    paddingTop: 10,
-    borderTopWidth: 1,
-    borderTopColor: "#ecf0f1",
-  },
-  mealItem: {
-    marginBottom: 8,
-  },
-  mealCategory: {
-    fontWeight: "500",
-    color: "#e74c3c",
-    fontSize: 16,
-  },
-  mealItems: {
-    color: "#34495e",
-    fontSize: 14,
-    marginLeft: 10,
-  },
-});
 
 export default MealPlanList;
