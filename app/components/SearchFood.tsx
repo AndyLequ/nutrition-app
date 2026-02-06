54fcc506f01a3ff68801df0f6d2dea5756b8341a
Andy Le <andylequ@gmail.com>
2026-02-05 21:46:36 -0600

    began adding selectedFood state back into SearchFood, removing it from the imported elements for useFoodSearch as well

--- app/components/SearchFood.tsx
+++ app/components/SearchFood.tsx
@@ -35,14 +35,15 @@ export const SearchFood = () => {
     searchQuery,
     searchResults,
     isSearching,
-    selectedFood,
-    setSelectedFood,
     handleSearch,
     clearResults,
   } = useFoodSearch();
 
   //the state variables, these states are concerned with the food being searched and then added
   const [amount, setAmount] = useState("");
+  const [selectedFood, setSelectedFood] = useState<UnifiedSearchResult | null>(
+    null,
+  );
   const [unit, setUnit] = useState("g");
   const [isFocused1, setIsFocused1] = useState(false);
   const [isFocused2, setIsFocused2] = useState(false);
